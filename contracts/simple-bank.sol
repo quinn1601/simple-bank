8081c0161037713bd92e4a2f8316476b7e0877c5
mukherjeearnab <mukherjeearnab905@gmail.com>
2020-02-05 20:02:48 +0530

    changed fallback message

--- contracts/simple-bank.sol
+++ contracts/simple-bank.sol
@@ -25,7 +25,7 @@ contract SimpleBank {
         owner = msg.sender;
     }
 
-    // Fallback function - Called if other functions don't match call
+    // Fallback Function - Called if other functions don't match call
     // or sent ether without data.
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
